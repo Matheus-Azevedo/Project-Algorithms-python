cae6a5e73ee9bd5e545b85067222cab156722a63
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-04-26 14:26:46 -0300

    feat: add Algoritmo de busca

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+
+def study_schedule(permanence_period, target_time) -> int:
+    if target_time is None or type(target_time) != int:
+        return None
+    count = 0
+    for start, end in permanence_period:
+        if start is None or end is None:
+            return None
+        if type(start) != int or type(end) != int:
+            return None
+        if start <= target_time <= end:
+            count += 1
+    return count
