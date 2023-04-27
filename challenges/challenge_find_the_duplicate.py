745946359d2444bbc4344b32bc8aa0d97ce9a179
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-04-27 00:06:19 -0300

    refact: complexities

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,6 +1,4 @@
 def find_duplicate(nums):
-    if not nums or isinstance(nums, str) or len(nums) <= 1:
-        return False
     nums.sort()
     for i in range(1, len(nums)):
         try:
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,9 @@
 def study_schedule(permanence_period, target_time) -> int:
-    if target_time is None or type(target_time) != int:
-        return None
     count = 0
     for start, end in permanence_period:
-        if start is None or end is None:
+        if start is None or end is None or target_time is None:
             return None
-        if type(start) != int or type(end) != int:
+        if type(start) != int or type(end) != int or type(target_time) != int:
             return None
         if start <= target_time <= end:
             count += 1
