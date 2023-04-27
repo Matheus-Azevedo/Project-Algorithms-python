34d24084b7a1998a3c8a2a5f00ffd70289932bda
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-04-26 23:53:25 -0300

    feat: challenge_find_the_duplicate

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
 def is_anagram(first_string, second_string):
-    ...
+    if len(first_string) == 0 or len(second_string) == 0:
+        return False
+
+    str1 = first_string.lower()
+    str2 = second_string.lower().reverse()
+
+    return str1 == str2
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,16 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or isinstance(nums, str) or len(nums) <= 1:
+        return False
+    nums.sort()
+    for i in range(1, len(nums)):
+        try:
+            nums[i] = int(nums[i])
+        except ValueError:
+            return False
+        else:
+            if nums[i] < 1:
+                return False
+            if nums[i] == nums[i-1]:
+                return nums[i]
+
+    return False
