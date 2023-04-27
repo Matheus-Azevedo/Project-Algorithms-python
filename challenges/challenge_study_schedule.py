b7f08677c644bbcda81a3cf8639389ee510604c6
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-04-27 00:18:55 -0300

    refact: complexities

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,8 @@ def study_schedule(permanence_period, target_time) -> int:
     for start, end in permanence_period:
         if start is None or end is None or target_time is None:
             return None
-        if isinstance(start, int) and isinstance(end, int) and start <= target_time <= end:
+        if isinstance(start, int) and isinstance(
+                end, int) and start <= target_time <= end:
             count += 1
         else:
             return None
