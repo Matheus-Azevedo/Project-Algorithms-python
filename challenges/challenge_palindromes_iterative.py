10f35535f3df5756c99c0fb1293bd21676de1801
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-04-26 23:55:05 -0300

    feat: challenge_palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """Check if a word is palindrome."""
+    if len(word) == 0:
+        return False
+    for i in range(len(word) // 2):
+        if word[i] != word[-(i + 1)]:
+            return False
+    return True
