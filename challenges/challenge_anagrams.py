363ac86113cfa76c1874a4f7e0af1de5ac73043b
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-04-27 01:22:08 -0300

    feat: anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,8 +1,39 @@
+# Agradecimentos a colega de turma: Aline
+
+def quick_sort(letters, start, end):
+    if start < end:
+        p = partition(letters, start, end)
+        quick_sort(letters, start, p - 1)
+        quick_sort(letters, p + 1, end)
+
+
+def partition(letters, start, end):
+    pivot = letters[end]
+    delimiter = start - 1
+
+    for index in range(start, end):
+        if letters[index] <= pivot:
+            delimiter = delimiter + 1
+            letters[index], letters[delimiter] = (
+                letters[delimiter],
+                letters[index],
+            )
+    letters[delimiter + 1], letters[end] = letters[end], letters[delimiter + 1]
+    return delimiter + 1
+
+
+def quick_sort_word(word):
+    letters = list(word.lower())
+    start = 0
+    end = len(letters) - 1
+    quick_sort(letters, start, end)
+    return "".join(letters)
+
+
 def is_anagram(first_string, second_string):
-    if len(first_string) == 0 or len(second_string) == 0:
-        return False
-
-    str1 = first_string.lower()
-    str2 = second_string.lower().reverse()
-
-    return str1 == str2
+    if not first_string and not second_string:
+        return (quick_sort_word(first_string),
+                quick_sort_word(second_string), False)
+    equal = quick_sort_word(first_string) == quick_sort_word(second_string)
+    return (quick_sort_word(first_string),
+            quick_sort_word(second_string), equal)
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -2,20 +2,28 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    # test TypeError for key
+    # Test - Verifica se a função lança TypeError se key não é um inteiro
     try:
-        encrypt_message("message", "key")
-    except TypeError as err:
-        assert str(err) == "tipo inválido para key"
+        encrypt_message("mensagem", "chave")
+    except TypeError:
+        pass
+    else:
+        assert False, "tipo inválido para key"
 
-    # test TypeError for message
+    # Test - Verifica se a função lança TypeError se message não é uma string
     try:
-        encrypt_message(123, 1)
-    except TypeError as err:
-        assert str(err) == "tipo inválido para message"
+        encrypt_message(123, 2)
+    except TypeError:
+        pass
+    else:
+        assert False, "tipo inválido para message"
 
-    # test key not in range
-    assert encrypt_message("message", 0) == "egassem"
+    assert encrypt_message("mensagem", 0) == "gnassemm"
+    assert encrypt_message("mensagem", -2) == "gnassemm"
+    assert encrypt_message("mensagem", 10) == "gnassemm"
 
-    # test key in range
-    assert encrypt_message("message", 1) == "egassem"
+    assert encrypt_message("mensagem", 3) == "nes_mgmassa"
+    assert encrypt_message("123456789", 3) == "789_456123"
+
+    assert encrypt_message("mensagem", 4) == "gmne_sassem"
+    assert encrypt_message("12345678", 4) == "5678_1234"
